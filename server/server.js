a0b4ec5967d5e4b3ec5e7132c831542de7142c71
kimkyeongnam <kkyy0126@naver.com>
2020-10-23 23:28:53 +0900

    Modify server addr

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1818;
 
 app.use(cors());
 app.use(express.json());
