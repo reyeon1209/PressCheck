b71972e2755bffdae9187e06a7de66e43a93bdcb
reyeon1209 <reyeon5368@naver.com>
2020-11-13 15:13:05 +0900

    Today's 화면 '시간별 키워드 추이' CSS 수정

--- client/src/components/Todays/TimeKeyword.js
+++ client/src/components/Todays/TimeKeyword.js
@@ -5,20 +5,19 @@ class TimeKeyword extends Component {
     static defaultProps = {
         info: {
             title: '0시 ~ 6시',
-            keyword: ['#언텍트1', '#메이플스토리m1', '#추석1', '#단풍1', '#미술관1']
+            keyword: ['언텍트1', '메이플스토리m1', '추석1', '단풍1', '미술관1']
         }
     }
 
     render() {
         // eslint-disable-next-line
         const { title, keyword } = this.props.info;
+        const keywordList = keyword.map((kw) => {return (<div className="timeKeyword-content">#{kw}</div>)})
 
         return (
             <div className='timeKeyword-table'>
                 <div className='timeKeyword-title'>{title}</div>
-                <div className='timeKeyword-content'>
-                    {keyword[0]}<br></br>{keyword[1]}<br></br>{keyword[2]}<br></br>{keyword[3]}<br></br>{keyword[4]}
-                </div>
+                {keywordList}
             </div>
         );
     }
