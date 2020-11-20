3170a42441b3c462d8a3428517eb5d0c466f5f34
kimkyeongnam <devokkn@gmail.com>
2020-11-20 22:05:45 +0900

    Del un-necessary files & Load data at Home menu(init)

--- client/src/components/Home/TopNewsInfo.js
+++ client/src/components/Home/TopNewsInfo.js
@@ -7,19 +7,26 @@ class TopNewsInfo extends Component {
     static defaultProps = {
         info: {
             id: 1,
-            title: '뉴스 제목',
-            thumnail: '기사 사진'
+            title: '준비 중입니다.....',
+            link: '',
+            img_src: 'https://www.flaticon.com/svg/static/icons/svg/1402/1402120.svg',
+            rank: 0,
+            category: '',
         }
     }
 
     render() {
-        const { id, title, thumnail } = this.props.info;
-
+        let { id, title, link, img_src, rank, category } = this.props.info;
+        if (id == "") {
+            title = '준비 중입니다...';
+            if (img_src == " ")
+                img_src = 'https://www.flaticon.com/svg/static/icons/svg/1402/1402120.svg';
+        }
         return (
             <div className='topnews-info'>
-                <div className='topnews-content_id'>{id}</div>
+                <div className='topnews-content_id'>{rank}</div>
                 <div className='topnews-content_title'>{title}</div>
-                <img className='topnews-thumbnail' src={thumnail} alt="thumnail" value="thumnail"/>
+                <img className='topnews-thumbnail' src={img_src} alt="thumnail" value="thumnail"/>
             </div>
         );
     }
--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -10,34 +10,24 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            date: new Date().getHours(),
             information: [
                 {
                     id: 1,
-                    title: '[속보] 주호명 "국방부가 추장관 지키는 추방부, 서씨 지키는 서방부 됐다',
-                    thumnail: 'https://imgmmw.mbn.co.kr/storage/news/2020/09/17/1374B399-A6F6-488C-A404-E31A38DF1BE1_576.jpg',
-                },
-                {
-                    id: 2,
-                    title: '"전화로 "위국헌신"하게 해달라"... 秋 아들, 안중근 비유에 "부글부글"',
-                    thumnail: 'https://img.mbn.co.kr/filewww/news/other/2020/09/17/990099100290.jpg',
-                },
-                {
-                    id: 3,
-                    title: '"파격" 신형 투싼, 경쟁차엔 "충격"...사전계약 첫날, 1만대 돌파',
-                    thumnail: 'https://file.mk.co.kr/meet/neds/2020/09/image_readtop_2020_960165_16003051264359396.jpg',
-                },
-                {
-                    id: 4,
-                    title: '윤지오가 소재불명? SNS에 버젓이 파티 영상 올렸다',
-                    thumnail: 'https://img.sbs.co.kr/newimg/news/20200917/201472123_1280.jpg',
-                },
-                {
-                    id: 5,
-                    title: '[속보] 서울 63-경기 54명-충남 10명-인천 7명-경북 6명 등 확진',
-                    thumnail: 'https://img8.yna.co.kr/photo/yna/YH/2020/09/16/PYH2020091616530001300_P2.jpg',
+                    title: '준비 중입니다.....',
+                    link: '',
+                    img_src: 'https://www.flaticon.com/svg/static/icons/svg/1402/1402120.svg',
+                    rank: 0,
+                    category: '',
                 }
             ]
         };
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:1818/mostRead')
+        .then(res => res.json())
+        .then(data => this.setState({information: data}));
     }
 
     render() {
@@ -47,7 +37,7 @@ class Home extends Component {
                     <SelectTable />
                     <div className='right-news-list'>
                         <div className='right-news-title'>가장 많이 본 뉴스 TOP 5</div>
-                        <div className='right-aggregate'>※ 0시 ~ 1시까지 집계한 뉴스리스트입니다.</div>
+                        <div className='right-aggregate'>※ 0시 ~ {this.state.date}시까지 집계한 뉴스리스트입니다.</div>
                         <TopNewsList data={this.state.information} />
                         <a className='more' href = '../news/newslist'>더보기 &gt;</a>
                     </div>
