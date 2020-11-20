fc40d625c6bdcfe09645c8c9441fbe287a8d9bf4
choi95 <41821494+choi95@users.noreply.github.com>
2020-11-21 03:15:40 +0900

    Add files via upload

--- flask/server.py
+++ flask/server.py
@@ -2,20 +2,21 @@
 from flask import Flask
 import pymongo
 from nltk.tokenize import sent_tokenize
-import Analysis as an
 from krwordrank.word import KRWordRank
 from konlpy.tag import Okt  
 import fasttext.util
 import numpy as np
 from apscheduler.schedulers.background import BackgroundScheduler
-
+from models import todays
+from models import Analysis as an
+from konlpy.tag import Hannanum
 
 app = Flask(__name__)
 client = pymongo.MongoClient("mongodb+srv://choi95:qpdlf52425@cluster0.kz4b2.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db = client.get_database('CapstonTest') 
 collection = db.ArticleTest
 
-fasttext.util.download_model('ko', if_exists='ignore')
+#fasttext.util.download_model('ko', if_exists='ignore')
 ft = fasttext.load_model('cc.ko.300.bin')
 
 word_dict = {}
@@ -23,6 +24,15 @@ strip_clean_sentence = []
 total_clean_sentence = []
 string_id = []
 
+def clean_text(text):
+    content = text
+    cleaned_text = re.sub('[a-zA-Z]','',content)
+    cleaned_text = re.sub('[\{\}\[\]\/?,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"▲△▽▼◁◀▷▶]', '', cleaned_text)
+    cleaned_text = cleaned_text.replace("연합뉴스TV 기사문의 및 제보","")
+    cleaned_text = cleaned_text.replace("카톡/라인 jebo23 (끝)","")
+    cleaned_text = cleaned_text.replace("아티클 공통  관련기사","")
+    cleaned_text = cleaned_text.replace("아티클 공통   250","")
+    return cleaned_text
 
 def insert_summary():
    
@@ -45,12 +55,12 @@ def insert_summary():
 
     temp=[]
     okt=Okt()
+    hannanum = Hannanum()
     for clean_sentence in total_clean_sentence:
         for s in clean_sentence:
-            tokenlist = okt.pos(s, stem=True, norm=True) # 단어 토큰화       
-            for word in tokenlist:
-                if word[1] in ["Noun"]: # 명사일 때만
-                    temp.append(word[0]) # 해당 단어를 저장함
+            noun = hannanum.nouns(s)       
+            for i in noun:
+                temp.append(i)
 
     for i in temp:
         word_vector_arr = np.asarray(ft[i],dtype='float32')
@@ -77,27 +87,59 @@ def insert_keyword():
     min_count = 3   # 단어의 최소 출현 빈도수 (그래프 생성 시)
     max_length = 10 # 단어의 최대 길이
     string_idx = 0
-    noun_keyword_list = []
-        
-    #for clean_sentence in total_clean_sentence:
+
+    stop_words = ['이', '있', '하', '것', '들', '그', '되', '수', '이', '보', '않', '없', '나', '사람', '주',
+             '아니', '등', '같', '우리', '때', '년', '가', '한', '지', '대하', '오', '말', '일',
+             '그렇', '위하']
+
     for clean_sentence in total_clean_sentence:
+        cleaned_sentence = []
+        noun_keyword_list = []
+        stop_keyword_list = []
         keyword_list = []
+        
         wordrank_extractor = KRWordRank(min_count=min_count, max_length=max_length)
         beta = 0.85
         max_iter = 10
-        keywords, rank, graph = wordrank_extractor.extract(clean_sentence, beta, max_iter) 
-        for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:]:
-            keyword_list.append(word)   
-        noun_keyword_list = okt.nouns(str(keyword_list))
-        collection.update_one({'_id': string_id[string_idx]},{'$set' : {'keyword' : noun_keyword_list[:]}})
-        keyword_list.clear()
+        
+        try:
+            keywords, rank, graph = wordrank_extractor.extract(clean_sentence, beta, max_iter)
+        except ValueError:
+            collection.update_one({'_id': string_id[string_idx]},{'$set' : {'keyword' : ''}})
+            string_idx += 1
+            continue
+        
+        for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:]:       
+            keyword_list.append(word)  
+
+        
+        for i in keyword_list:
+            a= okt.pos(i)    
+            if a[0][1] == 'Noun':
+                noun_keyword_list.append(a[0][0])
+
+        for i in noun_keyword_list:
+            if i not in stop_words:
+                stop_keyword_list.append(i)
+        if len(stop_keyword_list) == 0:
+            stop_keyword_list.append('')
+    
+        s1 = set(stop_keyword_list)
+        s1_list = list(s1)
+        
+        collection.update_one({'_id': string_id[string_idx]},{'$set' : {'keyword' : s1_list}})
         string_idx += 1
 
+'''
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(insert_summary, 'cron' ,minute="55")  #55분마다 함수실행
-sched.add_job(insert_keyword, 'cron' ,minute="55")
+sched.add_job(insert_summary, 'cron' ,minute="55")  #매 55분마다 함수실행
+sched.add_job(insert_keyword, 'cron' ,minute="55") 
+sched.add_job(todays.todaysTimes, 'cron' ,minute="55") #6시간단위
+sched.add_job(todays.todaysdictClear, 'cron' ,minute="55") #6시간단위
+sched.add_job(todays.todaysKeyword, 'cron' ,minute="55")  #6시간단위
+sched.add_job(todays.todaysClear, 'cron' ,minute="55")
 sched.start()
-
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
