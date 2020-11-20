f4a0fb8abb1e7314bd8fc4b47a062e546f60e244
reyeon1209 <reyeon5368@naver.com>
2020-11-20 17:34:27 +0900

    Analyze 화면 기사원문

--- client/src/components/Analyze/OriginalNewsText.js
+++ client/src/components/Analyze/OriginalNewsText.js
@@ -12,9 +12,9 @@ class OriginalNewsText extends Component {
 
     render() {
         // eslint-disable-next-line
-        const {title, uploaded, updated, editor, thumnail, content} = this.props.data;
+        const {title, uploaded, updated, editor, img_src, content} = this.props.data;
 
-        if (thumnail == '') {
+        if (img_src == '') {
             return (
                 <div>
                     <div className='content-news-title'><b>{title}</b></div>
@@ -30,7 +30,7 @@ class OriginalNewsText extends Component {
                     <div className='original-news-title'><b>{title}</b></div>
                     <div className='original-news-upload'>{uploaded}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{updated}</div>
                     <div>{editor}</div>
-                    <img className='original-news-thumbnail' src={thumnail} alt="thumnail" value="thumnail"/>
+                    <img className='original-news-thumbnail' src={img_src} alt="thumnail" value="thumnail"/>
                     <div className='original-news-text'>{content}</div> 
                 </div>
             );
