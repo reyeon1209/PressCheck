1fa469fda5fca2acee32ef5bd52650e1273d336a
kimkyeongnam <devokkn@gmail.com>
2020-11-20 22:51:14 +0900

    Success deploying menu

--- server/routes/mostRead.js
+++ server/routes/mostRead.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Model } = require('mongoose');
 let MostRead = require('../models/MostRead');
 
 router.route('/').get((req, res) => {
@@ -7,8 +8,16 @@ router.route('/').get((req, res) => {
     MostRead.find(variable)
         .exec((err, mostRead) => {
             if (err)  return  res.status(400).send(err);
-            res.status(200).json({ success: true, mostRead });
+            res.status(200).json(mostRead);
         });
 });
+
+router.route('/getId').get((req, res) => {
+    MostRead.find(variable).where('_id')
+        .exec((err, mostRead) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json(mostRead);
+        })
+})
 
 module.exports = router;
