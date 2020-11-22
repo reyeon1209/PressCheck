02139de68c48a22e50b764b743f53f8c8387b33b
choi95 <41821494+choi95@users.noreply.github.com>
2020-11-22 23:48:40 +0900

    Add files via upload

--- flask/server.py
+++ flask/server.py
@@ -88,47 +88,59 @@ def insert_keyword():
     max_length = 10 # 단어의 최대 길이
     string_idx = 0
 
-    stop_words = ['이', '있', '하', '것', '들', '그', '되', '수', '이', '보', '않', '없', '나', '사람', '주',
+    stop_words =  ['이', '있', '하', '것', '들', '그', '되', '수', '이', '보', '않', '없', '나', '사람', '주',
              '아니', '등', '같', '우리', '때', '년', '가', '한', '지', '대하', '오', '말', '일',
-             '그렇', '위하']
+             '그렇', '위하', '대한', '확', '관련', '이상', '미만', '경우', '텔레', '다시', '때문',
+             '제대로', '관', '지난', '비준', '지난해', '위해','곳곳', '현재', '당일', '주요', '일대', 
+              '요즘', '거기', '내', '지금', '정도', '이번', '처음', '모두', '통해'
+              , '더욱', '앞서', '진짜', '한번', '원래', '사실', '옆', '정말', '올해', '스',
+              '민', '초', '최근', '앞', '역시', '이후', '군', '최고', '가장', '중', '양', 
+              '대해', '사이', '얼마', '아주', '대비', '셈', '각국', '실거주','실수요자', '실',
+              '셀', '내년', '유독', '언제', '문득', '늘', '다른', '동안', '덩', 
+              '역시', '당시', '최', '변', '살', '이번', '점차', '건수','번', '쥴', '리', '상대로', '송', 
+              '이제', '매년', '곳', '오늘', '듯', '아무', '괜', '하나', '차지', '오히려', 
+              '순간', '속', '누군가','밥주','스마','문하','정유','주얼','좀더','먼저','디섐보','일주'
+              '섯알',  '가운데','보이' ,'것처', '해도' ,'대부분' ,'섯알' ,'먼저', '노', '해당', '거' ,'올레', '가가',
+              '씨' ,'랄라블', '대규모' ,'뭔가','디섐보', '퍼터', '김재', '종', '매사', '스스로', '하자',
+              '기', '날', '코로','물이','간사']
+
 
     for clean_sentence in total_clean_sentence:
         cleaned_sentence = []
-        noun_keyword_list = []
-        stop_keyword_list = []
-        keyword_list = []
-        
-        wordrank_extractor = KRWordRank(min_count=min_count, max_length=max_length)
-        beta = 0.85
-        max_iter = 10
-        
-        try:
-            keywords, rank, graph = wordrank_extractor.extract(clean_sentence, beta, max_iter)
-        except ValueError:
-            collection.update_one({'_id': string_id[string_idx]},{'$set' : {'keyword' : ''}})
-            string_idx += 1
-            continue
-        
-        for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:]:       
-            keyword_list.append(word)  
-
-        
-        for i in keyword_list:
-            a= okt.pos(i)    
-            if a[0][1] == 'Noun':
-                noun_keyword_list.append(a[0][0])
-
-        for i in noun_keyword_list:
-            if i not in stop_words:
-                stop_keyword_list.append(i)
-        if len(stop_keyword_list) == 0:
-            stop_keyword_list.append('')
-    
-        s1 = set(stop_keyword_list)
-        s1_list = list(s1)
-        
-        collection.update_one({'_id': string_id[string_idx]},{'$set' : {'keyword' : s1_list}})
+    noun_keyword_list = []
+    stop_keyword_list = []
+    keyword_list = []
+    wordrank_extractor = KRWordRank(min_count=min_count, max_length=max_length)
+    beta = 0.85
+    max_iter = 10
+  
+    try:
+        keywords, rank, graph = wordrank_extractor.extract(clean_sentence, beta, max_iter)
+    except ValueError:
+        collection.update_one({'_id': string_id[string_idx]},{'$set' : {'keyword' : ''}})
         string_idx += 1
+        continue
+
+    for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:]:       
+        keyword_list.append(word)  
+    for i in keyword_list:
+        a= okt.pos(i)    
+        if a[0][1] == 'Noun':
+            noun_keyword_list.append(a[0][0])
+
+    for i in noun_keyword_list:
+        if i not in stop_words:
+            stop_keyword_list.append(i)
+    if len(stop_keyword_list) == 0:
+        stop_keyword_list.append('')
+    
+    s1 = set(stop_keyword_list)
+    s1_list = list(s1)
+    s2_list = s1_list[:5]
+    
+    collection.update_one({'_id': string_id[string_idx]},{'$set' : {'keyword' : s1_list, 'point_keyword' : s2_list}})
+    string_idx += 1
+
 
 '''
 sched = BackgroundScheduler(daemon=True)
