a3cae5143f0f7254e8a77d55e26716c43863b5da
reyeon1209 <reyeon5368@naver.com>
2020-11-25 02:25:17 +0900

    Today's 화면 Tab 선택 이벤트 추가

--- client/src/components/Todays/Tabs.js
+++ client/src/components/Todays/Tabs.js
@@ -17,13 +17,13 @@ class Keyword extends Component {
         return (
             <div>
                 <ul className='tab-clear'>
-                    <li className='tabs' onClick={() => this.handleClickTab(0)}>전체</li>
-                    <li className='tabs' onClick={() => this.handleClickTab(1)}>정치</li>
-                    <li className='tabs' onClick={() => this.handleClickTab(2)}>사회</li>
-                    <li className='tabs' onClick={() => this.handleClickTab(3)}>경제</li>
-                    <li className='tabs' onClick={() => this.handleClickTab(4)}>국제</li>
-                    <li className='tabs' onClick={() => this.handleClickTab(5)}>스포츠</li>
-                    <li className='tabs' onClick={() => this.handleClickTab(6)}>문화</li>
+                    <li className='tabs' style={{ backgroundColor: this.state.activeTab == 0 ? "#FFFFFF" : "#EEEEEE" }} onClick={() => this.handleClickTab(0)}>전체</li>
+                    <li className='tabs' style={{ backgroundColor: this.state.activeTab == 1 ? "#FFFFFF" : "#EEEEEE" }} onClick={() => this.handleClickTab(1)}>정치</li>
+                    <li className='tabs' style={{ backgroundColor: this.state.activeTab == 2 ? "#FFFFFF" : "#EEEEEE" }} onClick={() => this.handleClickTab(2)}>사회</li>
+                    <li className='tabs' style={{ backgroundColor: this.state.activeTab == 3 ? "#FFFFFF" : "#EEEEEE" }} onClick={() => this.handleClickTab(3)}>경제</li>
+                    <li className='tabs' style={{ backgroundColor: this.state.activeTab == 4 ? "#FFFFFF" : "#EEEEEE" }} onClick={() => this.handleClickTab(4)}>국제</li>
+                    <li className='tabs' style={{ backgroundColor: this.state.activeTab == 5 ? "#FFFFFF" : "#EEEEEE" }} onClick={() => this.handleClickTab(5)}>스포츠</li>
+                    <li className='tabs' style={{ backgroundColor: this.state.activeTab == 6 ? "#FFFFFF" : "#EEEEEE" }} onClick={() => this.handleClickTab(6)}>문화</li>
                 </ul>
             </div>
         );
